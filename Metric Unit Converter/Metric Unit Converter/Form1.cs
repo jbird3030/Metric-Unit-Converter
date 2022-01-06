dd24cb720c484e0ae2a6146eb7c299641e7c2c93
jbird3030 <79331033+jbird3030@users.noreply.github.com>
2022-01-06 14:04:36 -0500

    more updates

--- Metric Unit Converter/Metric Unit Converter/Form1.cs
+++ Metric Unit Converter/Metric Unit Converter/Form1.cs
@@ -15,6 +15,15 @@ namespace Metric_Unit_Converter
         public Form1()
         {
             InitializeComponent();
+
+            buttonCToF.BackColor = Color.LightCyan;
+            buttonKToP.BackColor = Color.LightCyan;
+            buttonLToG.BackColor = Color.LightCyan;
+            buttonMToF.BackColor = Color.LightCyan;
+            buttonFToC.BackColor = Color.LightSkyBlue;
+            buttonPToK.BackColor = Color.LightSkyBlue;
+            buttonGToL.BackColor = Color.LightSkyBlue;
+            buttonFtoM.BackColor = Color.LightSkyBlue;
         }
 
         private void buttonClose_Click(object sender, EventArgs e)
@@ -33,6 +42,7 @@ namespace Metric_Unit_Converter
             {
                 double celsius = (fahrenheit - 32) * 5 / 9;
                 outputValue.Text = celsius.ToString();
+                outputValue.BackColor = Color.LightSkyBlue;
             }
             
 
@@ -56,6 +66,7 @@ namespace Metric_Unit_Converter
             {
                 double fahrenheit = ((celsius * 9 / 5) + 32);
                 outputValue.Text = fahrenheit.ToString();
+                outputValue.BackColor = Color.LightCyan;
             }
         }
 
@@ -72,6 +83,7 @@ namespace Metric_Unit_Converter
                 double meters = feet/3.28;
                 //string metersOut = meters.ToString();
                 outputValue.Text = String.Format($"{meters:n2}");
+                outputValue.BackColor = Color.LightSkyBlue;
             }
         }
 
@@ -88,6 +100,64 @@ namespace Metric_Unit_Converter
                 double feet = meters*3.28;
                 //string metersOut = meters.ToString();
                 outputValue.Text = String.Format($"{feet:n2}");
+                outputValue.BackColor = Color.LightCyan;
+            }
+        }
+
+        private void buttonPToK_Click(object sender, EventArgs e)
+        {
+            string poundsInput = inputValue.Text;
+            if (!Double.TryParse(poundsInput, out double pounds))
+            {
+                MessageBox.Show("That's not a valid entry.  Please enter a valid weight.");
+            }
+            else
+            {
+                double kilograms = pounds / 2.2;
+                outputValue.Text = String.Format($"{kilograms:n2}");
+                outputValue.BackColor = Color.LightSkyBlue;
+            }
+        }
+
+        private void buttonKToP_Click(object sender, EventArgs e)
+        {
+            string kilogramsInput = inputValue.Text;
+            if (!Double.TryParse(kilogramsInput, out double kilograms))
+            {
+                MessageBox.Show("That's not a valid entry.  Please enter a valid weight.");
+            } else
+            {
+                double pounds = kilograms * 2.2;
+                outputValue.Text = String.Format($"{pounds:n2}");
+                outputValue.BackColor = Color.LightCyan;
+            }
+        }
+
+        private void buttonGToL_Click(object sender, EventArgs e)
+        {
+            string inputGallons = inputValue.Text;
+            if (!Double.TryParse(inputGallons, out double gallons))
+            {
+                MessageBox.Show("That's not a valid entry.  Please enter a valid measurement.");
+            } else
+            {
+                double liters = gallons * 3.78;
+                outputValue.Text = String.Format($"{liters:n2}");
+                outputValue.BackColor = Color.LightSkyBlue;
+            }
+        }
+
+        private void buttonLToG_Click(object sender, EventArgs e)
+        {
+            string inputLiters = inputValue.Text;
+            if (!Double.TryParse(inputLiters, out double liters))
+            {
+                MessageBox.Show("That's not a valid entry.  Please enter a valid measurement.");
+            } else
+            {
+                double gallons = liters / 3.78;
+                outputValue.Text = String.Format($"{gallons:n2}");
+                outputValue.BackColor = Color.LightCyan;
             }
         }
     }
